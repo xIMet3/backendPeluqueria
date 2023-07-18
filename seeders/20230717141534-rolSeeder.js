0cbbe3de1cd61c1776267c7edc2a25c07a80bda5
xIMet3 <joaquin19_91@hotmail.com>
2023-07-18 14:09:03 +0200

    [feat]Added the roles of Admin, Hairdresser and User to the roleSeeder. // Añadido los roles de Admin, Peluquero y Usuario al rolSeeder.

--- seeders/20230717141534-rolSeeder.js
+++ seeders/20230717141534-rolSeeder.js
@@ -3,7 +3,22 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-   
+    await queryInterface.bulkInsert ('Rols', [
+      
+      {
+        id: 1,
+        name: "Admin"
+      },
+      {
+        id: 2,
+        name: "Peluquero"
+      },
+      {
+        id: 3,
+        name: "Usuario"
+      },
+
+    ], {});
   },
 
   async down (queryInterface, Sequelize) {
