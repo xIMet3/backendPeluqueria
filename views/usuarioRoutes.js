2f37bd2e4782f271364e95dd3f8f4632f693bd5a
xIMet3 <joaquin19_91@hotmail.com>
2023-07-27 13:51:15 +0200

    [update]Modified the routes of the endpoints verEmpleados and verServicios, removing the auth(middleware).

--- views/usuarioRoutes.js
+++ views/usuarioRoutes.js
@@ -7,8 +7,8 @@ router.get("/perfilUsuario", auth, usuarioController.getPerfil);
 // Ruta para modificar el perfil del usuario que esta logeado.
 router.put("/modificarPerfil", auth, usuarioController.updatePerfil);
 // Ruta para mostrar todos los empleados.
-router.get("/verEmpleados", auth, usuarioController.verEmpleados);
+router.get("/verEmpleados", usuarioController.verEmpleados);
 // Ruta para mostrar todos los servicios
-router.get("/verServicios", auth, usuarioController.verServicios);
+router.get("/verServicios", usuarioController.verServicios);
 
 module.exports = router;
